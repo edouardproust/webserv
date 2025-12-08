f8a1814fcb5d536a37a6322f586cc34cec93cecc
Edouard Proust <contact@edouardproust.dev>
2025-12-09 00:32:49 +0100

    todos for canonical clesses check

--- inc/cgi/CgiData.hpp
+++ inc/cgi/CgiData.hpp
@@ -4,7 +4,7 @@
 #include "config/LocationBlock.hpp"
 #include "config/HostPortPair.hpp"
 
-
+// TODO canonical class verification + justification message
 class CgiData
 {
 	bool	_isValid;
@@ -28,7 +28,6 @@ class CgiData
 
 	static std::string	_headerToEnvVar(std::string const&);
 
-	
 	CgiData();
 	CgiData&	operator=(CgiData const&);
 
